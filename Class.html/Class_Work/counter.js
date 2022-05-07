0754067595780cce39abbaff2d00536cb193d01a
Ankurak94 <ankurchumber2@gmail.com>
2022-05-07 23:31:44 +0530

    commit

--- Class.html/Class_Work/counter.js
+++ Class.html/Class_Work/counter.js
@@ -1,34 +1,14 @@
 var count = 0;
-// var counter = document.createElement(div);
-// counter.setAttribute('id','counter')
-// counter.setAttribute('class','counter')
-// let decBtn = document.createElement('button');
-// decBtn.innerText = '-';
-// decBtn.setAttribute('id','dec');
-// decBtn.setAttribute('class','btn');
-// let addBtn = document.createElement('button');
-// addBtn.innerText = '+';
-// addBtn.setAttribute('id','inc');
-// addBtn.setAttribute('class','btn');
-// let countText = document.createElement('span');
-// countText.innerText = count;
-// countText.setAttribute('id','count');
-// countText.setAttribute('class','count');
 
+let workarea    = getElem('div','counter','counter');
+let decBtn      = getElem('button','btn','dec','-');
+let counter_1   = getElem('span','counter','count',count);
+let incBtn      = getElem('button','btn','inc','+');
 
-// counter.append
+workarea.append(decBtn,counter_1,incBtn);
 
-var counterbody = document.createElement('div');
-let decBtn = document.createElement('button')
-decBtn.setAttribute('id','dec')
-decBtn.innerText = '-';
-let incBtn = document.createElement('button')
-incBtn.setAttribute('id','inc')
-incBtn.innerText = '+';
-let counter_1 = document.createElement('span')
-counter_1.setAttribute('id','count')
-counter_1.innerText = count;
-counterbody.append(decBtn,counter_1,incBtn);
+document.getElementById('counterbody').append(workarea);
+
 document.getElementById('inc').addEventListener('click',()=>{
     count ++;
     document.getElementById('count').innerText=count;
@@ -37,3 +17,11 @@ document.getElementById('dec').addEventListener('click',()=>{
     count --;
     document.getElementById('count').innerText=count;
 });
+
+function getElem(html_tag,class_tag,id_tag,text_value=''){
+    let elem = document.createElement(html_tag);
+    elem.setAttribute('class',class_tag);
+    elem.setAttribute('id',id_tag);
+    elem.innerText = text_value;
+    return elem;
+}
